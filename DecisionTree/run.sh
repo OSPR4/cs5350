b34688c3c230936a2457d198f36e37967cbdce47
OSPR4 <68337818+OSPR4@users.noreply.github.com>
2023-09-23 23:26:49 -0600

    shell script added

---
+++ DecisionTree/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script runs main.py for the Decision Tree question in HW1.
+# To run this script, navigate to the directory where the script is located and run the following command:
+# chmod +x run.sh
+# ./run.sh
+
+
+python3 main.py
