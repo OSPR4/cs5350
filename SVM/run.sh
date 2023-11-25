5fe095fb1c1a260e9bf409daa46a129acd0f3fc3
OSPR4 <68337818+OSPR4@users.noreply.github.com>
2023-11-25 10:27:13 -0700

    Add SVM implementation with primal and dual variants

---
+++ SVM/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script runs main.ipynb for Perceptron.
+# To run this script, navigate to the directory where the script is located and run the following command:
+# chmod +x run.sh
+# ./run.sh
+# This will execute the notebook and export the results in a file called 'main.nbconvert.ipynb' in the current directory.
+
+
+jupyter nbconvert --execute --to notebook main.ipynb
